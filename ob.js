fb9499440b26f9bebb2eb777660e0f6de2cf3d5d
Rakhi-kashyap <101251859+Rakhikahyap@users.noreply.github.com>
2022-11-18 23:06:14 +0530

    Add files via upload

---
+++ ob.js
@@ -0,0 +1,31 @@
+// object creation
+// const person = { 
+//     name: 'John',
+//     age: 20
+// };
+// console.log(typeof person);
+
+
+// var person ={name:'rakhi',age:'21',class:'b.s.c'};
+// console.log(person)
+
+
+// accsin Object
+// const person = { name: 'John', age: 20, };
+// accessing property
+// '''console.log(person.name)'''
+
+var vegetables ={
+    veg1:"potato",
+    veg2:"brinjal",
+    veg3:"bottle gourd"
+ }
+  
+ var vegetable1= vegetables.veg1;
+ var vegetable2= vegetables.veg2;
+ var vegetable3= vegetables.veg3;
+ 
+ 
+ console.log(vegetable1);
+ console.log(vegetable2);
+ console.log(vegetable3);
