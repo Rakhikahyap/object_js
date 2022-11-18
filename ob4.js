3e2cec70559d96aa291d2a10aa863b9699101ee5
Rakhi-kashyap <101251859+Rakhikahyap@users.noreply.github.com>
2022-11-18 23:07:58 +0530

    Add files via upload

---
+++ ob4.js
@@ -0,0 +1,11 @@
+// d1 = {'a': 100, 'b': 200, 'c':300}
+// d2 = {'a': 300, 'b': 200, 'd':400}
+
+let a = {
+    name : 'rakhi',
+    lastname: 'kashayap',
+    age : 25,
+    email : 'hello@yahoobaba.net'
+
+}
+console.log(a);
