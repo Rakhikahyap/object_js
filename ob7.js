92e3538f4e478382579b49528d2670ee9b8274b4
Rakhi-kashyap <101251859+Rakhikahyap@users.noreply.github.com>
2022-11-18 23:19:49 +0530

    Add files via upload

---
+++ ob7.js
@@ -0,0 +1,12 @@
+// list={1:['bhopal'],2:['punjab'],3:['delhi'],4:['kanpur']}
+// a={}
+// b=[]
+// for (i in list){
+//     x=list[i]
+//     for(j in x){
+//         a[i]=j
+//     }
+
+// }
+// console.log(a)
+
